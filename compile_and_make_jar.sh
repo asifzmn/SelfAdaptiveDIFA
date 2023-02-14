9e4300ef36eddcfe4244a4fbe5dfec3fe7862b9c
asif <asifzaman2215s@gmail.com>
2023-02-13 21:13:50 -0800

    compile_and_make_jar.sh for compiling f

--- compile_and_make_jar.sh
+++ compile_and_make_jar.sh
@@ -1,2 +1,4 @@
+$JAVA_HOME/bin/javac -cp "/pool/home/asif/SelfAdaptiveDIFA/tool/soot-trunk.jar:/pool/home/asif/SelfAdaptiveDIFA/tool/FLOWDIST.jar:" dtUtil.java
+$JAVA_HOME/bin/javac -cp "/pool/home/asif/SelfAdaptiveDIFA/tool/soot-trunk.jar:/pool/home/asif/SelfAdaptiveDIFA/tool/FLOWDIST.jar:" dtUtil.java dtConfig.java
 $JAVA_HOME/bin/javac -cp "/pool/home/asif/SelfAdaptiveDIFA/tool/soot-trunk.jar:/pool/home/asif/SelfAdaptiveDIFA/tool/FLOWDIST.jar:" dtUtil.java dtConfig.java ReadSourceSinkWritePairFiles.java
 $JAVA_HOME/bin/javac -cp "/pool/home/asif/SelfAdaptiveDIFA/tool/soot-trunk.jar:/pool/home/asif/SelfAdaptiveDIFA/tool/FLOWDIST.jar:/pool/home/asif/SelfAdaptiveDIFA/tool/DUA1.jar:" dtUtil.java  dtOptions.java ComputeRelevantMethods.java
